0c0e4c048a290b8076b2ed1fde86cf86e93d1199
permito <penciller94@gmail.com>
2024-02-18 14:06:13 +0100

    Avl tests, equivalent to Bst

--- avl/avl_test.go
+++ avl/avl_test.go
@@ -25,14 +25,72 @@ func BenchmarkAvl_PutSequence(b *testing.B) {
 	}
 }
 
-func BenchmarkAvl_Contains(b *testing.B) {
+func BenchmarkAvl_ContainsRandom(b *testing.B) {
+	arr := make([]int, b.N)
 	avl := New[int, int]()
 	for i := 0; i < b.N; i++ {
-		avl.Put(rand.Int(), i)
+		key := rand.Int()
+		avl.Put(key, i)
+		arr[i] = key
 	}
+	rand.Shuffle(b.N, func(i, j int) {
+		arr[i], arr[j] = arr[j], arr[i]
+	})
 
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		avl.Contains(i)
+		avl.Contains(arr[i])
+	}
+}
+
+func BenchmarkAvl_ContainsSequence(b *testing.B) {
+	arr := make([]int, b.N)
+	avl := New[int, int]()
+	for i := 0; i < b.N; i++ {
+		avl.Put(i, i)
+		arr[i] = i
+	}
+	rand.Shuffle(b.N, func(i, j int) {
+		arr[i], arr[j] = arr[j], arr[i]
+	})
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		avl.Contains(arr[i])
+	}
+}
+
+func BenchmarkAvl_DeleteRandom(b *testing.B) {
+	arr := make([]int, b.N)
+	avl := New[int, int]()
+	for i := 0; i < b.N; i++ {
+		key := rand.Int()
+		avl.Put(key, i)
+		arr[i] = key
+	}
+	rand.Shuffle(b.N, func(i, j int) {
+		arr[i], arr[j] = arr[j], arr[i]
+	})
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		avl.Delete(arr[i])
+	}
+}
+
+func BenchmarkAvl_DeleteSequence(b *testing.B) {
+	arr := make([]int, b.N)
+	avl := New[int, int]()
+	for i := 0; i < b.N; i++ {
+		avl.Put(i, i)
+		arr[i] = i
+	}
+	rand.Shuffle(b.N, func(i, j int) {
+		arr[i], arr[j] = arr[j], arr[i]
+	})
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		avl.Delete(arr[i])
 	}
 }
